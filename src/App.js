9bf88c44066d12bb3a3ab150c9009e855f007d6a
Jinbread <jinjae.lee@icloud.com>
2019-06-26 15:10:06 +0200

    rm device orientation component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <header className="App-header">
 
         <Compass />
+        <Locator />
       </header>
     </div>
   );
--- src/compass.js
+++ src/compass.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Frame } from "framer"
-import DeviceOrientation from 'react-device-orientation';
 
 // Open Preview (CMD + P)
 // API Reference: https://www.framer.com/api
@@ -21,16 +20,6 @@ export function Compass() {
     return (
         <Frame center backgroundColor={"transparent"} y={-40}>
             {alpha.toFixed(1)}
-            <DeviceOrientation>
-                {({ absolute, alpha, beta, gamma }) => (
-                <div>
-                    {`Absolute: ${absolute}`}
-                    {`Alpha: ${alpha}`}
-                    {`Beta: ${beta}`}
-                    {`Gamma: ${gamma}`}
-                </div>
-                )}
-            </DeviceOrientation>
         </Frame>
     )
 }
